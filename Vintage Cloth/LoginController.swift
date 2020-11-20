e630be0ca5377992b321155a7d5f90dffe295e9f
doomy <dahyun@kakao.com>
2020-11-21 01:11:53 +0900

    Initial Commit

---
+++ Vintage Cloth/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vintage Cloth
+//
+//  Created by 다현 on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Vintage ClothTests/Vintage_ClothTests.swift
@@ -0,0 +1,33 @@
+//
+//  Vintage_ClothTests.swift
+//  Vintage ClothTests
+//
+//  Created by 다현 on 2020/11/21.
+//
+
+import XCTest
+@testable import Vintage_Cloth
+
+class Vintage_ClothTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
